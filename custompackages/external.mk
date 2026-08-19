ab020406388b7d35cc6eec61e42553f5fea3338c
flll <2844835+flll@users.noreply.github.com>
2026-08-19 18:46:26 +0900

    fix: expat 2.8.2 に XML_DEV_URANDOM を渡す
    カーネル 3.10 / glibc 2.24 には getrandom が無く、イメージ rebuild が xmlparse.c で落ちた。

--- custompackages/external.mk
+++ custompackages/external.mk
@@ -1,1 +1,5 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_ATOMCAM_TOOLS_PATH)/package/*/*.mk))
+
+# Linux 3.10 + glibc 2.24 には getrandom が無い。expat 2.8.2 はエントロピー源が
+# 無いと xmlparse.c で #error する。/dev/urandom を使う。
+EXPAT_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -DXML_DEV_URANDOM"
